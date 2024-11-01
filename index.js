4f364658add319684a2c9af1e17497649e382b34
RSGameTech <rsgame0604@gmail.com>
2024-11-02 01:48:55 +0530

    Small CSS change

--- index.js
+++ index.js
@@ -1,96 +1,4 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
 let genPass = document.querySelector(".generate-btn");
 let passwordBox = document.querySelectorAll(".password-box");
@@ -101,63 +9,58 @@ passwordBox[0].textContent = "Pass";
 passwordBox[1].textContent = "Pass";
 
 let passwords = (num) => {
-  let parse = "";
-  for (let i = 0; i < num; i++) {
-    char = characters[Math.floor(Math.random() * characters.length)];
-    if (!parse.includes(char)) {
-      parse += char;
+    let parse = "";
+    for (let i = 0; i < num; i++) {
+        char = characters[Math.floor(Math.random() * characters.length)];
+        if (!parse.includes(char)) {
+            parse += char;
+        }
     }
-  }
-  return parse;
+    return parse;
 };
 genPass.addEventListener("click", () => {
-  passwordBox[0].textContent = passwords(12);
-  passwordBox[1].textContent = passwords(12);
+    passwordBox[0].textContent = passwords(12);
+    passwordBox[1].textContent = passwords(12);
 });
 
 let copiedPasswords = JSON.parse(localStorage.getItem("copiedPasswords")) || [];
 
 const updatePrevPass = () => {
-  prevPass.innerHTML = copiedPasswords
-    .map((pass) => `<div>${pass}</div>`)
-    .join("");
+    prevPass.innerHTML = copiedPasswords.map((pass) => `<div class="password-list">${pass}</div>`).join("");
 };
 
 updatePrevPass();
 
 document.querySelectorAll(".password-boxx img").forEach((img) => {
-  img.addEventListener("click", () => {
-    let box = img.parentElement.querySelector(".password-box");
-    navigator.clipboard.writeText(box.textContent);
+    img.addEventListener("click", () => {
+        let box = img.parentElement.querySelector(".password-box");
+        navigator.clipboard.writeText(box.textContent);
 
-    if (
-      copiedPasswords.includes(box.textContent) ||
-      box.textContent === "Pass"
-    ) {
-      return 0;
-    } else {
-      if (copiedPasswords.length === 5) {
-        copiedPasswords.shift();
-      }
-      copiedPasswords.push(box.textContent);
-    }
+        if (copiedPasswords.includes(box.textContent) || box.textContent === "Pass") {
+            return 0;
+        } else {
+            if (copiedPasswords.length === 5) {
+                copiedPasswords.shift();
+            }
+            copiedPasswords.push(box.textContent);
+        }
 
-    localStorage.setItem("copiedPasswords", JSON.stringify(copiedPasswords));
-    updatePrevPass();
+        localStorage.setItem("copiedPasswords", JSON.stringify(copiedPasswords));
+        updatePrevPass();
 
-    img.src = "tick.svg";
-    img.style.filter = "invert(1)";
-    setTimeout(() => {
-      img.src = "copy.svg";
-      img.style.filter = "invert(0)";
-    }, 1000);
-  });
+        img.src = "tick.svg";
+        img.style.filter = "invert(1)";
+        setTimeout(() => {
+            img.src = "copy.svg";
+            img.style.filter = "invert(0)";
+        }, 1000);
+    });
 });
 
 theme.addEventListener("change", () => {
-  document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("light-mode");
 });
 
 document.body.addEventListener("change", () => {
-  prevPass.textContent = JSON.parse(localStorage.getItem("copiedPasswords"));
+    prevPass.textContent = JSON.parse(localStorage.getItem("copiedPasswords"));
 });
